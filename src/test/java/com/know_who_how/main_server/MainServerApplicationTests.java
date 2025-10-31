458d40c6f348484208df07a6871649635030ec0b
blueoxygens <exboxer234@gmail.com>
2025-10-31 09:24:55 +0900

    [chore] 프로젝트 세팅

---
+++ src/test/java/com/know_who_how/main_server/MainServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.know_who_how.main_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
