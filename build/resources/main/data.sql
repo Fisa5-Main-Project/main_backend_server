7683353acb49ef9c52dce4fdc478b79985b05985
uxxin <dbwlsdid1114@naver.com>
2025-11-17 14:56:22 +0900

    [feat] 일자리 ErrorCode 추가

---
+++ build/resources/main/data.sql
@@ -0,0 +1,22 @@
+-- Term (약관)
+INSERT INTO term (is_required, term_name) VALUES (true, '서비스 이용약관');
+INSERT INTO term (is_required, term_name) VALUES (true, '개인정보 수집 및 이용 동의');
+INSERT INTO term (is_required, term_name) VALUES (false, '마케팅 정보 수신 동의');
+
+-- Keyword (키워드)
+INSERT INTO keyword (name) VALUES ('안정적 생활비');
+INSERT INTO keyword (name) VALUES ('목돈 마련');
+INSERT INTO keyword (name) VALUES ('비상금 확보');
+INSERT INTO keyword (name) VALUES ('증여/상속');
+INSERT INTO keyword (name) VALUES ('대출 상환');
+INSERT INTO keyword (name) VALUES ('여행');
+INSERT INTO keyword (name) VALUES ('가족/교류');
+INSERT INTO keyword (name) VALUES ('고급 취미');
+INSERT INTO keyword (name) VALUES ('반려동물');
+INSERT INTO keyword (name) VALUES ('귀농/귀촌');
+INSERT INTO keyword (name) VALUES ('창업/사업');
+INSERT INTO keyword (name) VALUES ('재취업/소일거리');
+INSERT INTO keyword (name) VALUES ('자기계발');
+INSERT INTO keyword (name) VALUES ('봉사/사회공헌');
+INSERT INTO keyword (name) VALUES ('건강/의료비');
+INSERT INTO keyword (name) VALUES ('편안한 휴식');
