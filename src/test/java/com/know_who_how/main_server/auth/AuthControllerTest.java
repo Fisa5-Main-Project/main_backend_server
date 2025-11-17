7fb87eacf59b9501f2a0f62bb0617d87fc013b25
Lee Jong Hyuck <101847527+Jhcki222@users.noreply.github.com>
2025-11-17 17:47:39 +0900

    Test : Auth 테스트 코드 작성

--- src/test/java/com/know_who_how/main_server/auth/AuthControllerTest.java
+++ src/test/java/com/know_who_how/main_server/auth/AuthControllerTest.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -54,7 +55,7 @@ class AuthControllerTest {
     @MockBean
     private SmsCertificationService smsCertificationService;
 
-    @MockBean
+    @SpyBean
     private JwtProperties jwtProperties;
 
     @Test
