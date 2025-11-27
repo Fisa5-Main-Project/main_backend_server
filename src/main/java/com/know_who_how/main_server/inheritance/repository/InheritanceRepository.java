e7e4d63f60a1a926de97e696618d5f2b796f5b77
uxxin <dbwlsdid1114@naver.com>
2025-11-27 17:57:17 +0900

    [feat] 상속 repository 생성

---
+++ src/main/java/com/know_who_how/main_server/inheritance/repository/InheritanceRepository.java
@@ -0,0 +1,8 @@
+package com.know_who_how.main_server.inheritance.repository;
+
+import com.know_who_how.main_server.global.entity.Inheritance.Inheritance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InheritanceRepository extends JpaRepository<Inheritance, Integer> {
+
+}
---
+++ src/main/java/com/know_who_how/main_server/inheritance/repository/InheritanceVideoRepository.java
@@ -0,0 +1,8 @@
+package com.know_who_how.main_server.inheritance.repository;
+
+import com.know_who_how.main_server.global.entity.Inheritance.InheritanceVideo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InheritanceVideoRepository extends JpaRepository<InheritanceVideo, Integer> {
+
+}
